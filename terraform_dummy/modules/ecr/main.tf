1178b83b54a18a159017393ad6d4908458e4c33a
Erik <erik.raboczki98@gmail.com>
2025-10-16 08:46:17 +0200

    add: further ENV fixes in Jenkinsfile and add IAM User creation but it's commented out

--- terraform_dummy/modules/ecr/main.tf
+++ terraform_dummy/modules/ecr/main.tf
@@ -74,3 +74,43 @@ resource "aws_iam_role_policy_attachment" "jenkins_ecr_policy_attachment" {
   role       = aws_iam_role.jenkins_ecr_role.name
   policy_arn = aws_iam_policy.jenkins_ecr_policy.arn
 }
+
+
+
+
+
+# resource "aws_iam_user" "jenkins_user" {
+#   name = "jenkins-user"
+# }
+
+# resource "aws_iam_group" "jenkins_group" {
+#   name = "jenkins-group"
+# }
+
+# resource "aws_iam_policy" "assume_jenkins_ecr_role_policy" {
+#   name        = "AssumeJenkinsEcrRolePolicy"
+#   description = "Allows assuming only the jenkins_ecr_role"
+
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect   = "Allow",
+#         Action   = "sts:AssumeRole",
+#         Resource = aws_iam_role.jenkins_ecr_role.arn
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_group_policy_attachment" "jenkins_group_policy_attach" {
+#   group      = aws_iam_group.jenkins_group.name
+#   policy_arn = aws_iam_policy.assume_jenkins_ecr_role_policy.arn
+# }
+
+# resource "aws_iam_user_group_membership" "jenkins_user_group_membership" {
+#   user = aws_iam_user.jenkins_user.name
+#   groups = [
+#     aws_iam_group.jenkins_group.name
+#   ]
+# }
